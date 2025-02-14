32d79abfcad6e240f5967c3293a96fb93fc044a4
Aayush Jha <aayush4jha@gmail.com>
2025-02-15 00:42:52 +0530

    CORS fix

--- backend/index.js
+++ backend/index.js
@@ -75,6 +75,20 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_DOMAIN);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle OPTIONS request
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
+
+
 app.listen(PORT, () => {
   console.log(`App is running at ${PORT}`);
 });
