bfb32ba8220f78f58d5da2617d305fe78513b3e5
Aayush Jha <aayush4jha@gmail.com>
2025-02-15 01:04:05 +0530

    fixed frontend api route

--- frontend/src/services/authService.jsx
+++ frontend/src/services/authService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const authenticateWithGoogle = async (token) => {
   try {
-    const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/auth/google`, {
+    const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/auth/google`, {
       token
     });
     // console.log(response, token);
